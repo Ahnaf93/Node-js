43f6d0d1f1de072d5eb8f3ec7adae18332e6408f
cis101 <cis101@secs.iub.edu.bd>
2019-07-08 09:44:35 +0600

    classwork -8

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 var http = require('http');
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello World\n");
-  });
-  server.listen(process.env.PORT, process.env.IP, function(){
+var express=require('express');
+var app=express();
+var server=http.Server(app)
+var fs=require('fs');
+
+ // var server = http.createServer(function(req, res){
+   // res.statusCode = 200;
+   // res.setHeader('Content-Type', 'text/html');
+    //fs.readFile('index.html', function(err,data){
+
+    //  if(err){
+     //   return console.log("file read error")
+     // }
+      
+     // res.end(data);
+
+   // })
+   
+  //});
+
+  app.get('/', function(req, res){
+
+    res.sendFile(__dirname+'/index.html')
+  })
+
+
+  app.get('/about/test:id', function(req, res){
+
+    res.sendFile(__dirname+'/second.html')
+  })
+  
+
+  app.get('/about/second', function(req, res){
+
+    res.sendFile(__dirname+'/second.html')
+  })
+  
+
+  
+  server.listen(3000, 'localhost', function(){
     console.log('Server running');
   });
