2a1b4137c67262038cce16e78355410b023fef54
cis101 <cis101@secs.iub.edu.bd>
2019-07-10 09:33:18 +0600

    classwork 8c

--- server.js
+++ server.js
@@ -1,43 +1,49 @@
+//Task- 8b,c
 var http = require('http');
-var express=require('express');
-var app=express();
-var server=http.Server(app)
-var fs=require('fs');
-
- // var server = http.createServer(function(req, res){
-   // res.statusCode = 200;
-   // res.setHeader('Content-Type', 'text/html');
-    //fs.readFile('index.html', function(err,data){
-
-    //  if(err){
-     //   return console.log("file read error")
-     // }
-      
-     // res.end(data);
-
-   // })
-   
-  //});
-
+var express = require('express')
+var app = express ();
+//var fs = require('fs');
+var server = http.Server(app)
+var bodyParser=require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+//(8a)
+  // var server = http.createServer(function(req, res){
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-Type', 'text/html');
+  //   fs.readFile('index.html',function(err,data){
+  //     if(err){
+  //       return console.log("File not found")
+  //     }
+  //     res.end(data);
+  //   })
+  // });
   app.get('/', function(req, res){
-
     res.sendFile(__dirname+'/index.html')
   })
 
-
-  app.get('/about/test:id', function(req, res){
-
-    res.sendFile(__dirname+'/second.html')
+//8c                                                                                                                   
+  app.get('/form', function(req, res){
+    res.sendFile(__dirname+'/form.html')
   })
-  
 
+
+
+  // app.get('/about/:id', function(req, res){
+    //if(req.params){
+      //res.json({key:'value'})
+    //}
+  //   res.sendFile(__dirname+'/index.html')
+  // })
   app.get('/about/second', function(req, res){
-
     res.sendFile(__dirname+'/second.html')
   })
-  
 
-  
+  //8c
+  app.post('/article/new', function(req,res){
+    console.log(req.body)
+    res.send({message:"data received"})
+  })
   server.listen(3000, 'localhost', function(){
     console.log('Server running');
   });
